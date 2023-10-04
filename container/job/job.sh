5cdbe3e62b647a4035068101a211657a86e8c336
Martin <github@msturm.net>
2023-10-04 20:52:31 +0200

    add killer

--- container/job/job.sh
+++ container/job/job.sh
@@ -7,7 +7,7 @@ STATUS=$1
 
 # Enter your pipeline steps below
 # Syntax: $RUNSTEP $STATUS <DESCRIPTION> <COMMAND>
-$RUNSTEP $STATUS "Step 1" "sleep 3"
-$RUNSTEP $STATUS "Step 2" "sleep 2"
-$RUNSTEP $STATUS "Step 3" "sleep 3"
-$RUNSTEP $STATUS "Step 4" "sleep 10"
+$RUNSTEP $STATUS "Step 1" "/job/my_important_step.sh 500"
+$RUNSTEP $STATUS "Step 2" "/job/my_important_step.sh 2"
+$RUNSTEP $STATUS "Step 3" "/job/my_important_step.sh 3"
+$RUNSTEP $STATUS "Step 4" "/job/my_important_step.sh 4"
--- container/src/killer.sh
+++ container/src/killer.sh
@@ -12,6 +12,25 @@ CANCELLED_FILE=$TRIGGER_DIR/cancel.flag
 WWW_USER=www-data
 ###############################################
 
+kill_all()
+{    
+   local PID=$1
+   local TASK_IDS=$(ls /proc/$PID/task)
+   for TASK_ID in $TASK_IDS
+   do
+      if [ -e /proc/$PID/task/$TASK_ID/children ]
+      then
+         local CHILDREN=$(cat /proc/$PID/task/$TASK_ID/children)
+         for CHILD in $CHILDREN
+         do     
+            kill_all $CHILD
+         done
+      fi
+   done
+   kill $PID
+}
+
+
 while true; do
     if [ ! -f $CANCELLED_FILE ] 
     then
@@ -20,7 +39,7 @@ while true; do
     if [ -f $CANCELLED_FILE ]
     then
         rm $CANCELLED_FILE 
-        killall job.sh
+        kill_all $(pgrep job.sh)
     fi
     sleep 1
 done
